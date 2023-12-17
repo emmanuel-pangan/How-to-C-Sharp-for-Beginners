7498d698d8f737dc1d642e36aaba2f7c3e635d0b
Emmanuel Pangan <145820526+pangan-dev@users.noreply.github.com>
2023-12-17 10:44:59 +0800

    Add files via upload

---
+++ (Bonus Part) What are the Basics of Comments in C# (Single-line and Multi-line)/Program.cs
@@ -0,0 +1,29 @@
+// Single-line Comment
+Console.WriteLine("Above");
+// Whatever you want.
+Console.WriteLine("Below");
+
+// Calculates the next leap year.
+var nextLeapYear = DateTime.Now.AddYears(4); // Description here.
+
+// TODO: 1. Optimize this code. 2. Google or go to StackOverflow. 3. Copy-paste the code. 4. Refactor again.
+
+/*
+ * Multi
+ * Line
+ * Comment
+ */
+
+/*
+ * And just type anything inside here.
+ * And you're good to go!
+ * 
+ */
+
+/*
+ * TODO: 
+ * 1. Optimize this code. 
+ * 2. Google or go to StackOverflow. 
+ * 3. Copy-paste the code. 
+ * 4. Refactor again.
+ */
