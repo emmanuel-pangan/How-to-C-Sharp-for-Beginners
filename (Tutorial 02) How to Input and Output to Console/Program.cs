a63aa4327f33e9e73c867eb349ca7571b71d0ae1
Emmanuel Pangan <145820526+pangan-dev@users.noreply.github.com>
2023-12-17 10:38:26 +0800

    Add files via upload

---
+++ (Tutorial 02) How to Input and Output to Console/Program.cs
@@ -0,0 +1,20 @@
+// Console.Write
+
+Console.Write("Hello World!");
+
+// Console.WriteLine
+
+Console.WriteLine("Hello World");
+Console.WriteLine();
+Console.WriteLine("Hello World");
+
+// Console.ReadKey
+
+Console.WriteLine("What's your name?");
+Console.ReadKey();
+
+// Console.ReadLine
+
+Console.WriteLine("What's your favorite color?");
+string? color = Console.ReadLine();
+Console.WriteLine("My favorite color is " + color);
