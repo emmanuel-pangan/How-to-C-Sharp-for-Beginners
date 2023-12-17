aade729e471f6cf2dae6aaea1fce8d7e3055d3fc
Emmanuel Pangan <145820526+pangan-dev@users.noreply.github.com>
2023-12-17 10:46:07 +0800

    Create SampleCode.cs

---
+++ (Bonus Part) What are the Basics of Comments in C# (Single-line and Multi-line)/SampleCode.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SampleCode
+    {
+        //public double GetFruitPrice(string fruitName)
+        //{
+        //    const int FRUIT_NAME_NOT_FOUND = -1;
+        //    switch (fruitName)
+        //    {
+        //        case "Apple":
+        //            return 5.99;
+        //        case "Banana":
+        //            return 3.99;
+        //        case "Grapes":
+        //            return 7.99;
+        //    }
+        //    return FRUIT_NAME_NOT_FOUND;
+        //}
+    }
+}
